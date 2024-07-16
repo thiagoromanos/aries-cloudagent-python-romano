e92b190b58a1ca6ad96366dede49568e46abc2bc
Thiago Romano <112953503+thiagoromanos@users.noreply.github.com>
2024-07-16 15:41:29 -0300

    Update valid.py
    Signed-off-by: Thiago Romano <112953503+thiagoromanos@users.noreply.github.com>

--- aries_cloudagent/messaging/valid.py
+++ aries_cloudagent/messaging/valid.py
@@ -489,12 +489,12 @@ class IndyRevRegId(Regexp):
         rf"([{B58}]{{21,22}}):3:"
         rf"CL:(([1-9][0-9]*)|([{B58}]{{21,22}}:2:.+:[0-9.]+))(:.+)?:"
         rf"CL_ACCUM:(.+$)|^((did:indy2)?:.+:[{B58}]{{21,22}})"  # issuer DID
-            f"/anoncreds/v0/REV_REG/"  # cred def id marker
+            f"\/anoncreds\/v0\/REV_REG_DEF\/"  # cred def id marker
             rf"((did:indy2)?:.+:[{B58}]{{21,22}})"  # issuer DID
-            f"/anoncreds/v0/CLAIM_DEF/"  # cred def id marker
+            f"\/anoncreds\/v0\/CLAIM_DEF\/"  # cred def id marker
             # f":CL"  # sig alg
-            rf"((did:indy2)?:.+:[{B58}]{{21,22}}/anoncreds/v0/SCHEMA/.+/[0-9.]+)"  # schema txn / id
-            f"/(.+)?(:|/)CL_ACCUM(:|/).+$"
+            rf"((did:indy2)?:.+:[{B58}]{{21,22}}\/anoncreds\/v0\/SCHEMA\/.+/[0-9.]+)"  # schema txn / id
+            f"\/(.+)?(:|\/)CL_ACCUM(:|\/).+$"
     )
 
     def __init__(self):
